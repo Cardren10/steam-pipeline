a9df7c63dd6ae84377692f84e12b5fe955b09409
cardren10 <carderorene@gmail.com>
2025-02-05 16:06:11 -0500

    created wrapper for python dictionary get method that handles null values.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -6,3 +6,13 @@ def db_conn():
     return psycopg2.connect(
         f"dbname={constants.DATABASE_NAME} user={constants.DATABASE_USER} password={constants.DATABASE_PASSWORD} host={constants.DATABASE_ENDPOINT} port={constants.DATABASE_PORT}"
     )
+
+
+def get_handle_null(dict: dict, str: str):
+    if dict == None:
+        return None
+    if str not in dict:
+        return None
+    if dict["str"] == None:
+        return None
+    return dict.get(f"{str}")
