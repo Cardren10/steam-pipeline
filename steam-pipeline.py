d603fb9c9e94eb1593ebce3727b83f586a2c41b6
cardren10 <carderorene@gmail.com>
2025-03-08 11:09:22 -0500

    added logging for pipeline time to complete.

--- steam-pipeline.py
+++ steam-pipeline.py
@@ -1,12 +1,24 @@
 from pipelines.steam.steam_extractor import get_app_data
 from pipelines.steam.steam_loader import load_data
+import helpers
+import time
+import logging
 
 
 def main():
-    print("Getting app data.")
+    logger = logging.getLogger("steam-pipeline")
+    helpers.setup_logger()
+    logger.debug("debug")
+
+    start = time.time()
+
+    logging.debug("Getting app data.")
     get_app_data()
-    print("loading data into schema.")
+    logging.debug("loading data into schema.")
     load_data()
+
+    end = time.time()
+    logging.debug(f"pipeline ran for {end - start} seconds")
 
 
 if __name__ == "__main__":
