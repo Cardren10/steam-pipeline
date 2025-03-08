a0aee06abac026f2b5fbf5e02b4f0a8f8e168d76
cardren10 <carderorene@gmail.com>
2025-03-08 14:12:47 -0500

    Turned pipeline stages into classes. Added more data sources.

--- pipelines/steam/steam_extractor.py
+++ pipelines/steam/steam_extractor.py
@@ -1,77 +1,169 @@
 from datetime import datetime, timezone
-from helpers import db_conn, setup_logger, validate_json
+import helpers
 import logging
 import requests
 import json
 import time
 
 
-def get_already_collected_apps():
-    """gets all of the apps currently in the postgres database"""
-    conn = db_conn()
-    cursor = conn.cursor()
-    query = "SELECT app_id FROM steam_landing"
-    cursor.execute(query)
-    records = [i[0] for i in cursor.fetchall()]
-    conn.close()
-    return records
+class SteamExtractor:
 
+    def execute(self):
+        """Runs the extractor."""
+        logger = logging.getLogger("steam-pipeline")
+        helpers.setup_logger()
+        logger.debug("debug")
 
-def get_app_list():
-    """Get app list."""
-    url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-    try:
-        response = requests.get(url)
-    except requests.exceptions.RequestException as e:
-        raise Exception(e)
-    content = response.content
-    content_json = json.loads(content)
-    apps = content_json["applist"]["apps"]
-    appids = [app["appid"] for app in apps]
-    return appids
+        logging.debug("getting app details.")
+        self.get_app_details()
+        logging.debug("getting app reviews.")
+        self.get_app_reviews()
+        logging.debug("getting app tags.")
+        self.get_app_tags()
 
+    def get_already_collected_apps(self, source: str) -> list:
+        """gets all of the apps currently in the postgres database"""
+        conn = helpers.db_conn()
+        cursor = conn.cursor()
+        query = cursor.mogrify(
+            """
+            SELECT app_id
+            FROM steam_landing
+            WHERE source = %s
+            """,
+            (source),
+        )
+        cursor.execute(query)
+        records = [i[0] for i in cursor.fetchall()]
+        conn.close()
+        return records
 
-def filtered_appids():
-    """create a list of uncollected ids"""
-    in_sql_set = set(get_already_collected_apps())
-    appids_set = set(get_app_list())
-    return list(appids_set - in_sql_set)
-
-
-def get_app_data():
-    """loop through app ids to retrieve app details and send it to the database"""
-    logger = logging.getLogger("steam-pipeline")
-    setup_logger()
-    logger.debug("debug")
-
-    appids = filtered_appids()
-
-    conn = db_conn()
-    cursor = conn.cursor()
-
-    for id in appids:
-        url = f"https://store.steampowered.com/api/appdetails?appids={id}"
+    def get_app_list(self, url: str) -> list:
+        """Get app list."""
         try:
             response = requests.get(url)
         except requests.exceptions.RequestException as e:
             raise Exception(e)
-        data = json.dumps(response.content.decode("utf-8-sig"))
-        if not validate_json(data):
-            logging.warning(f"{id} returned non valid json skipping id.")
-            continue
-        logging.debug(f"uploading {id}")
-        query = cursor.mogrify(
-            "INSERT INTO steam_landing (app_id, app_data, app_source, timestamp, transformed) VALUES (%s, %s, %s, %s, %s)",
-            (
-                id,
-                data,
-                "steam_api",
-                datetime.now(timezone.utc).isoformat(),
-                "0",
-            ),
-        )
-        cursor.execute(query)
-        conn.commit()
-        time.sleep(1.5)
+        content = response.content
+        content_json = json.loads(content)
+        apps = content_json["applist"]["apps"]
+        appids = [app["appid"] for app in apps]
+        return appids
 
-    conn.close()
+    def filtered_appids(self, source: str, url: str) -> list:
+        """create a list of uncollected ids"""
+        in_sql_set = set(self.get_already_collected_apps(source))
+        appids_set = set(self.get_app_list(url))
+        return list(appids_set - in_sql_set)
+
+    def get_app_details(self) -> None:
+        """loop through app ids to retrieve app details and send it to the database"""
+        appids = self.filtered_appids(
+            "steam_api_appdetails",
+            "https://api.steampowered.com/ISteamApps/GetAppList/v2/",
+        )
+
+        conn = helpers.db_conn()
+        cursor = conn.cursor()
+
+        for id in appids:
+            url = f"https://store.steampowered.com/api/appdetails?appids={id}"
+            try:
+                response = requests.get(url)
+            except requests.exceptions.RequestException as e:
+                raise Exception(e)
+            data = json.dumps(response.content.decode("utf-8-sig"))
+            if not helpers.validate_json(data):
+                logging.warning(f"{id} returned non valid json skipping id.")
+                continue
+            logging.debug(f"uploading app details for {id}")
+            query = cursor.mogrify(
+                "INSERT INTO steam_landing (app_id, app_data, app_source, timestamp, transformed) VALUES (%s, %s, %s, %s, %s)",
+                (
+                    id,
+                    data,
+                    "steam_api_appdetails",
+                    datetime.now(timezone.utc).isoformat(),
+                    "0",
+                ),
+            )
+            cursor.execute(query)
+            conn.commit()
+            time.sleep(1.5)
+
+        conn.close()
+
+    def get_app_reviews(self) -> None:
+        """loop through app ids to retrieve app reviews and send it to the database"""
+
+        appids = self.filtered_appids(
+            "steam_api_appreviews",
+            "https://api.steampowered.com/ISteamApps/GetAppList/v2/",
+        )
+
+        conn = helpers.db_conn()
+        cursor = conn.cursor()
+
+        for id in appids:
+            url = f"https://store.steampowered.com/appreviews/{id}"
+            try:
+                response = requests.get(url)
+            except requests.exceptions.RequestException as e:
+                raise Exception(e)
+            data = json.dumps(response.content.decode("utf-8-sig"))
+            if not helpers.validate_json(data):
+                logging.warning(f"{id} returned non valid json skipping id.")
+                continue
+            logging.debug(f"uploading reviews for {id}")
+            query = cursor.mogrify(
+                "INSERT INTO steam_landing (app_id, app_data, app_source, timestamp, transformed) VALUES (%s, %s, %s, %s, %s)",
+                (
+                    id,
+                    data,
+                    "steam_api_appreviews",
+                    datetime.now(timezone.utc).isoformat(),
+                    "0",
+                ),
+            )
+            cursor.execute(query)
+            conn.commit()
+            time.sleep(1.5)
+
+        conn.close()
+
+    def get_app_tags(self):
+        """loop through app ids to retrieve app tags and send it to the database"""
+
+        appids = self.filtered_appids(
+            "steamspy_tags", "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+        )
+
+        conn = helpers.db_conn()
+        cursor = conn.cursor()
+
+        for id in appids:
+            url = f"https://steamspy.com/api.php?request=appdetails&appid={id}"
+            try:
+                response = requests.get(url)
+            except requests.exceptions.RequestException as e:
+                raise Exception(e)
+            data = json.dumps(response.content.decode("utf-8-sig"))
+            if not helpers.validate_json(data):
+                logging.warning(f"{id} returned non valid json skipping id.")
+                continue
+            logging.debug(f"uploading tags for {id}")
+            query = cursor.mogrify(
+                "INSERT INTO steam_landing (app_id, app_data, app_source, timestamp, transformed) VALUES (%s, %s, %s, %s, %s)",
+                (
+                    id,
+                    data,
+                    "steamspy_tags",
+                    datetime.now(timezone.utc).isoformat(),
+                    "0",
+                ),
+            )
+            cursor.execute(query)
+            conn.commit()
+            time.sleep(1.1)
+
+        conn.close()
--- steam-pipeline.py
+++ steam-pipeline.py
@@ -1,5 +1,5 @@
-from pipelines.steam.steam_extractor import get_app_data
-from pipelines.steam.steam_loader import load_data
+from pipelines.steam.steam_extractor import SteamExtractor
+from pipelines.steam.steam_loader import SteamLoader
 import helpers
 import time
 import logging
@@ -13,9 +13,9 @@ def main():
     start = time.time()
 
     logging.debug("Getting app data.")
-    get_app_data()
+    SteamExtractor.execute()
     logging.debug("loading data into schema.")
-    load_data()
+    SteamLoader.execute()
 
     end = time.time()
     logging.debug(f"pipeline ran for {end - start} seconds")
